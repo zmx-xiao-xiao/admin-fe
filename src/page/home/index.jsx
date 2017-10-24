3ea6d1b8ff6df7864f738b8d0ee59e9a4f3d501a
zmx-xiao-xiao <zmx_1321673345@qq.com>
2017-10-24 22:04:24 +0800

    v

---
+++ src/page/home/index.jsx
@@ -0,0 +1,30 @@
+'use strict';
+import React        from 'react';
+import ReactDOM     from 'react-dom';
+
+import PageTitle    from 'component/page-title/index.jsx';
+
+const Home = React.createClass({
+    getInitialState() {
+        return {
+            hello: 'Welcome'
+        };
+    },
+    componentDidMount: function(){
+       console.log('Home did mount');
+    },
+    render() {
+        return (
+            <div id="page-wrapper">
+                <PageTitle pageTitle="Home"/>
+                <div className="row">
+                    <div className="col-lg-3 col-md-6">
+                        Welcome
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+export default Home;
